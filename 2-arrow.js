abcdad20bdc12447424168159d09aa3a754031bf
Allan Mwenja <140613367+mahianyuallan@users.noreply.github.com>
2024-06-24 08:31:35 +0300

    Delete 2-arrow.js

--- 2-arrow.js
+++
@@ -1,9 +0,0 @@
-export default class getNeighborhoodsList {
-  constructor() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-    this.addNeighborhood = (newNeighborhood) => {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return this.sanFranciscoNeighborhoods;
-    };
-  }
-}
